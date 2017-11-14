25fe311bc2cf4af4bc79b358fb019e8cd8566b09
marcpre <marcus.presich@gmail.com>
2017-11-14 16:12:34 +0000

    setting up project structure

--- src/app.js
+++ src/app.js
@@ -4,10 +4,6 @@ const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const passport = require('passport')
-
-const index = require('./routes/index')
 
 const app = express()
 
@@ -17,20 +13,22 @@ app.set('view engine', 'pug')
 app.use(logger(process.env.LOG_ENV))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: false,
+    extended: false,
 }))
 app.use(express.static(path.join(__dirname, '/../public')))
 app.use(cookieParser())
 
 // routes
-app.use('/', index)
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 // Start Server
 const port = process.env.APP_PORT || 8080
 const host = process.env.APP_URL || '0.0.0.0'
 
 app.listen(port, host, () => {
-  console.log(`Listening on ${host}:${port}/login`)
+    console.log(`Listening on ${host}:${port}/`)
 })
 
 module.exports = app
