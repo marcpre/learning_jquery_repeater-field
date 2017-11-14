6ef1ce6baf6e03390b7d7d3daa7353236cbe7e72
marcpre <marcus.presich@gmail.com>
2017-11-14 19:56:07 +0100

    adding resources and fixing paths in hbs

--- src/app.js
+++ src/app.js
@@ -9,26 +9,26 @@ const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('view engine', 'hbs')
 app.use(logger(process.env.LOG_ENV))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended: false,
+  extended: false,
 }))
-app.use(express.static(path.join(__dirname, '/../public')))
+app.use(express.static(path.join(__dirname, '/../public/')))
 app.use(cookieParser())
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.render('index')
 })
 
 // Start Server
 const port = process.env.APP_PORT || 8080
-const host = process.env.APP_URL || '0.0.0.0'
+const host = process.env.APP_URL || 'localhost'
 
 app.listen(port, host, () => {
-    console.log(`Listening on ${host}:${port}/`)
+  console.log(`Listening on ${host}:${port}/`)
 })
 
 module.exports = app
