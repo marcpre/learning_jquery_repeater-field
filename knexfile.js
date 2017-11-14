0ff8276e292ad26aa77ad20dc95feb4cc512b865
marcpre <marcus.presich@gmail.com>
2017-11-14 20:19:54 +0100

    adding migrations

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,16 @@
-// Update with your config settings.
+//load env variables
+require("dotenv").config()
 
+// Update with your config settings.
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    development: {
+        client: process.env.DB_CONNECTION,
+        connection: {
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        },
+    },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+}
